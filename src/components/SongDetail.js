ce70c63c7884accbf1e79b563eeb5b6100a3a9ba
Clucas0311 <clucas0311@gmail.com>
2021-02-07 16:32:05 -0500

    Added Conditional Rendering to Song Selection

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-	console.log(props);
-	return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+	//CONDITIONAL RENDERING:
+	// When we first render out our component we don't have yet selectedSong/props.title
+	// We are trying to read a prop of null so to fix this we will check it
+	// if song doesn't exist yet
+	if (!song) {
+		// prompt the user to select a song/ click on something
+		return <div>Select a song</div>;
+	}
+
+	return (
+		<div>
+			<h3>Details for:</h3>
+			<p>
+				Title: {song.title}
+				<br />
+				Duration : {song.duration}
+			</p>
+		</div>
+	);
 };
 // we use mapStateToProps to reach into that state object
 const mapStateToProps = (state) => {
 	// we are going to reach into that state object and pull out only the properties we need
 	// to get the selected song we will need to access state.selectedSong
+	// if you want to customize the key you will do it here:
 	return { song: state.selectedSong };
 };
 
